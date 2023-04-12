7f18c3048efe8894d34f821e45aa0d5892e33e10
kingsleykbc <kingsleybestconcept@gmail.com>
2023-04-12 02:20:48 +0100

    Adjusted items

--- src/features/Highlights/SummaryTooltip/SummaryTooltip.tsx
+++ src/features/Highlights/SummaryTooltip/SummaryTooltip.tsx
@@ -6,50 +6,18 @@ import Text from '../../../components/Text/Text';
 
 const SummaryTooltip: React.FC = () => {
 	const [selectedText, setSelectedText] = useState('');
-	const [position, setPosition] = useState<{ x: number; y: number } | null>(null);
 	const { disabled, generateSummary, loading } = useHighlights();
 	const tooltipRef = useRef<HTMLDivElement>(null);
 
 	const handleMouseUp = (e: MouseEvent) => {
 		if (disabled || (tooltipRef.current && tooltipRef.current.contains(e.target as Node))) return;
 		const text = window.getSelection()?.toString().trim() || '';
-
 		if (text) {
 			setSelectedText(text);
-			setPosition({ x: e.pageX, y: e.pageY });
 		} else {
 			setSelectedText('');
-			setPosition(null);
 		}
 	};
-
-	useLayoutEffect(() => {
-		if (selectedText && position && tooltipRef.current) {
-			const tooltipWidth = tooltipRef.current.clientWidth;
-			const tooltipHeight = tooltipRef.current.clientHeight;
-			const windowWidth = window.innerWidth;
-			const windowHeight = window.innerHeight;
-
-			let x = position.x;
-			let y = position.y;
-
-			let updatePosition = false;
-
-			if (x + tooltipWidth > windowWidth) {
-				x = windowWidth - tooltipWidth;
-				updatePosition = true;
-			}
-
-			if (y + tooltipHeight > windowHeight) {
-				y = windowHeight - tooltipHeight;
-				updatePosition = true;
-			}
-
-			if (updatePosition) {
-				setPosition({ x, y });
-			}
-		}
-	}, [selectedText, position]);
 
 	useEffect(() => {
 		setSelectedText('');
@@ -59,7 +27,7 @@ const SummaryTooltip: React.FC = () => {
 		};
 	}, [disabled]);
 
-	if (!selectedText || !position) {
+	if (!selectedText) {
 		return null;
 	}
 
@@ -69,14 +37,7 @@ const SummaryTooltip: React.FC = () => {
 	};
 
 	return (
-		<div
-			ref={tooltipRef}
-			className={styles.summaryTooltip}
-			style={{
-				left: `${position.x}px`,
-				top: `${position.y}px`
-			}}
-		>
+		<div ref={tooltipRef} className={styles.summaryTooltip}>
 			<Text size='sm' className={styles.selectedText}>
 				<Text size='sm' color='gray'>
 					Selected:{' '}
